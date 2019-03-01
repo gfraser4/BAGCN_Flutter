b6cd2c2c55f09ce5c819a12e56868b7d727593ab
abutler10 <abutler10@ncstudents.niagaracollege.ca>
2019-03-01 15:57:54 -0500

    typo fixed

--- lib/About/aboutPage.dart
+++ lib/About/aboutPage.dart
@@ -21,7 +21,7 @@ class AboutPage extends StatelessWidget {
                       ),
                       
                       Text(
-                        '\n\nWelcome to the Boys and Girls Club Messaging App. This app allows you to stay up to date with the latest anouncements based on your specific class enrollment. It also allows yur to quickly communicate with the instructor.',
+                        '\n\nWelcome to the Boys and Girls Club Messaging App. This app allows you to stay up to date with the latest anouncements based on your specific class enrollment. It also allows you to quickly communicate with the instructor.',
                         style: TextStyle(fontSize: 18), textAlign: TextAlign.center,
                       ),
                     ],
