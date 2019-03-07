154c77c18abf17dc7653b31e3687eff29e2de7f4
Graeme Fraser <45044460+gfraser4@users.noreply.github.com>
2019-03-06 19:45:38 -0500

    Added custom colors and starting changing the login page

---
+++ lib/Style/customColors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class CustomColors {
+  CustomColors._(); 
+  static const Color bagcGreen = Color.fromRGBO(123, 193, 67, 1);
+  static const Color bagcBlue = Color.fromRGBO(28, 165, 229, 1);
+}
